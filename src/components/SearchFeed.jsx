6f0b66f0e6a9c5f0932c2e958645205a7b18e579
Naeem Sani <Naeem.Sani@fdmgroup.com>
2023-01-10 08:51:09 +0800

    first commit

---
+++ src/components/SearchFeed.jsx
@@ -0,0 +1,25 @@
+import { useEffect, useState } from "react";
+import { Box } from "@mui/material";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos } from "./";
+import { useParams } from "react-router-dom";
+
+const SearchFeed = () => {
+  const [videos, setVideos] = useState(null);
+  const { searchTerm } = useParams()
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+    }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
+
+export default SearchFeed;
